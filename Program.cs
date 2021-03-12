165c4b071d7b407b89cec4a1221ffcb84e69130e
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-03-12 23:06:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение стороны a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int p = 4 * a;
+            Console.WriteLine($"Периметр равен {p} ");
+            Console.ReadKey();
+        }
+    }
+}
