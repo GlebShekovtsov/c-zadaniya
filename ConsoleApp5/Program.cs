cbf33d52e05da9b4a261314d65cb64a768b64a89
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-03-14 22:37:25 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int v = a*a*a;
+            Console.WriteLine($"Площадь составляет {v} см в кубе");
+            int s = 4 * a*a;
+            Console.WriteLine($"Площадь боковой поверхности составляет {s} см в квадрате");
+            Console.ReadKey();
+        }
+    }
+}
