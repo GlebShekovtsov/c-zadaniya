281b950c4bede0cde0e3741c92e6364cff3b5ba7
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-03-14 22:28:48 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество метров");
+            int metr = Convert.ToInt32(Console.ReadLine());
+            int kilo = metr * 1000;
+            Console.WriteLine($" Это будет {kilo} километров");
+            Console.ReadKey();
+        }
+    }
+}
